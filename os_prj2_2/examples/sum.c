3b7a717586a02baa0abf3451cb84354dadd0d85e
dward1001 <dward1001@users.noreply.github.com>
2018-04-05 18:19:52 +0900

    Add project2_2 files via upload

---
+++ os_prj2_2/examples/sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syscall.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  if (argc < 5)
+	return EXIT_FAILURE;
+
+  int a = atoi(argv[1]),
+	  b = atoi(argv[2]),
+	  c = atoi(argv[3]),
+	  d = atoi(argv[4]);
+
+  printf("%d %d\n", fibonacci(a), sum_of_four_integers(a, b, c, d));
+
+  return EXIT_SUCCESS;
+}
